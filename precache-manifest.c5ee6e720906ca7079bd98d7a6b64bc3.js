d9ea1047d3a6fc548b31bb4f043c143c02328ce3
beans3 <beans3.k@gmail.com>
2020-08-06 13:02:22 +0900

    Updates

---
+++ precache-manifest.c5ee6e720906ca7079bd98d7a6b64bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0edbedeb78d31dea6327a2b295bf894f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "5d3a21f9dde7fedb5b11",
+    "url": "/movie_app_2020/static/css/main.d9048615.chunk.css"
+  },
+  {
+    "revision": "8b7ab456ca2673ac934a",
+    "url": "/movie_app_2020/static/js/2.09240959.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.09240959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3a21f9dde7fedb5b11",
+    "url": "/movie_app_2020/static/js/main.78d0f2c6.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
